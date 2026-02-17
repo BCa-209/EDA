379c092ae36733f4ccc0f46eec9721cf553e31f9
BCa-209 <brayancalderon.20014@gmail.com>
2026-02-16 23:12:58 -0500

    menu

--- PlanoC/plano.cpp
+++ PlanoC/plano.cpp
@@ -1,57 +1,76 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+#include <vector>
 using namespace std;
 
 class Plano {
 private:
-    int maxX;  // Horizontal (eje X)
-    int maxY;  // Vertical   (eje Y)
+    int maxX;  // Horizontal
+    int maxY;  // Vertical
+
+    struct Punto {
+        int x;
+        int y;
+    };
+
+    vector<Punto> puntos;
 
 public:
 
     void limites(int x, int y) {
-        maxX = x;  // ancho
-        maxY = y;  // alto
+        maxX = x;
+        maxY = y;
     }
 
-    void input(int posX, int posY) {
+    void agregarPunto(int x, int y) {
 
-        // Validación de límites
-        if(posX < 0 || posX > maxX || posY < 0 || posY > maxY) {
-            cout << "Coordenadas fuera de rango\n";
+        if(x < 0 || x > maxX || y < 0 || y > maxY) {
+            cout << "Punto (" << x << "," << y << ") fuera de rango\n";
             return;
         }
 
-        // Recorre eje vertical (Y)
+        puntos.push_back({x, y});
+    }
+
+    void draw() {
+
         for(int y = maxY; y >= 0; y--) {
-            // Recorre eje horizontal (X)
+
             for(int x = 0; x <= maxX; x++) {
-                // Esquina de los ejes
+
                 if(y == 0 && x == 0) {
                     cout << "   +";
                 }
-                // Eje horizontal
                 else if(y == 0) {
                     cout << "--";
                 }
-                // Eje vertical
                 else if(x == 0) {
                     cout << setw(2) << y << " |";
                 }
-                // Punto X
-                else if(x == posX && y == posY) {
-                    cout << " X";
-                }
-                // Espacio vacío
                 else {
-                    cout << " .";
+
+                    bool hayPunto = false;
+
+                    for(const auto& p : puntos) {
+                        if(p.x == x && p.y == y) {
+                            hayPunto = true;
+                            break;
+                        }
+                    }
+
+                    if(hayPunto)
+                        cout << " X";
+                    else
+                        cout << " .";
                 }
             }
+
             cout << endl;
         }
 
         // Numeración eje X
-        cout << " 0  ";
+        cout << "    ";
         for(int x = 1; x <= maxX; x++) {
             if(x % 2 == 0)
                 cout << setw(2) << x;
@@ -60,30 +79,127 @@ public:
         }
         cout << endl;
     }
+
+    void pntCercano(int x, int y) {
+        if(puntos.empty()) {
+            cout << "No hay puntos en el plano\n";
+            return;
+        }
+
+        Punto masCercano = puntos[0];
+        double minDist = sqrt(pow(masCercano.x - x, 2) + pow(masCercano.y - y, 2));
+
+        for(const auto& p : puntos) {
+            double dist = sqrt(pow(p.x - x, 2) + pow(p.y - y, 2)); //raiz((x2-x1)^2 + (y2-y1)^2)
+            if(dist < minDist) {
+                minDist = dist;
+                masCercano = p;
+            }
+        }
+
+        cout << "Punto mas cercano a (" << x << "," << y << ") es (" 
+            << masCercano.x << "," << masCercano.y << ") con distancia " 
+            << minDist << endl;
+    }
+
+
 };
 
+void clean() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
 int main() {
+    Plano plano;
+    int opcion;
 
-    Plano planito;
+    do {
+        clean();
+        cout << "==== MENU PLANO CARTESIANO ====\n";
+        cout << "1. Definir limites del plano\n";
+        cout << "2. Agregar punto\n";
+        cout << "3. Mostrar plano\n";
+        cout << "4. Punto mas cercano\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione opcion: ";
+        cin >> opcion;
 
-    planito.limites(30, 10);  // 20 en X, 10 en Y
+        switch(opcion) {
+            case 1: {
+                int x, y;
+                cout << "Ingrese limite X: ";
+                cin >> x;
+                cout << "Ingrese limite Y: ";
+                cin >> y;
+                plano.limites(x, y);
+                break;
+            }
+            case 2: {
+                int px, py;
+                cout << "Ingrese coordenada X del punto: ";
+                cin >> px;
+                cout << "Ingrese coordenada Y del punto: ";
+                cin >> py;
+                plano.agregarPunto(px, py);
+                plano.draw();
+                cout << "Punto agregado. Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 3: {
+                plano.draw();
+                cout << "Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 4: {
+                int px, py;
+                cout << "Ingrese coordenada X de referencia: ";
+                cin >> px;
+                cout << "Ingrese coordenada Y de referencia: ";
+                cin >> py;
+                plano.pntCercano(px, py);
+                cout << "Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 5:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opcion invalida. Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+        }
 
-    planito.input(2, 3);
-    cout << endl;
-    planito.input(15, 7);
-    cout << endl;
+    } while(opcion != 5);
 
     return 0;
 }
 
-
 // TODO KNN- vecinos mas cercanos
 
 
-    //void draw(int posX = -1, int posY = -1) {
-    //    input(posY, posX);
-    //}
-    /*
+/*
+plano.agregarPunto(2, 3); 
+plano.agregarPunto(15, 7); 
+plano.agregarPunto(10, 5); 
+plano.agregarPunto(1, 9);
+plano.pntCercano(5, 5);
+
+
+    void draw(int posX = -1, int posY = -1) {
+        input(posY, posX);
+    }
+    
     void draw() {
         for(int i=ejeX; i>=0; i--) {
             for(int j=ejeY; j>=0; j--) {
@@ -113,4 +229,4 @@ int main() {
         }
         cout << endl;
     }
-    */
+*/
--- PlanoC/plano_gotoxy.cpp
+++ PlanoC/plano_gotoxy.cpp
@@ -1,48 +1,162 @@
 #include <iostream>
-#include <windows.h>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <windows.h> // Para gotoxy
 using namespace std;
 
-void gotoxy(int x, int y) {
-    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD dwPos;
-    dwPos.X = x;
-    dwPos.Y = y;
-    SetConsoleCursorPosition(hCon, dwPos);
-}
+class Plano {
+private:
+    int maxX;  // Horizontal
+    int maxY;  // Vertical
 
-void punto(int x, int y, int origenX, int origenY) {
-    gotoxy(origenX + x, origenY - y);
-    cout << "X";
+    struct Punto {
+        int x;
+        int y;
+    };
+
+    vector<Punto> puntos;
+
+    // Función interna para dibujar en coordenadas específicas
+    void gotoxy(int x, int y) {
+        COORD coord;
+        coord.X = x;
+        coord.Y = y;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    }
+
+public:
+
+    void limites(int x, int y) {
+        maxX = x;
+        maxY = y;
+    }
+
+    void agregarPunto(int x, int y) {
+        if(x < 0 || x > maxX || y < 0 || y > maxY) {
+            cout << "Punto (" << x << "," << y << ") fuera de rango\n";
+            return;
+        }
+        puntos.push_back({x, y});
+    }
+
+    void draw() {
+        // Dibujar ejes
+        for(int y = 0; y <= maxY; y++) {
+            gotoxy(0, maxY - y + 1); // +1 para dejar fila 0 para el menú
+            cout << setw(2) << y << " |";
+        }
+
+        for(int x = 1; x <= maxX; x++) {
+            gotoxy(3 + x*2, maxY + 2); // +2 para dejar margen arriba
+            if(x % 2 == 0) cout << x;
+            else cout << " ";
+        }
+
+        // Dibujar puntos
+        for(const auto& p : puntos) {
+            gotoxy(3 + p.x*2, maxY - p.y + 1);
+            cout << "X";
+        }
+    }
+
+    void pntCercano(int x, int y) {
+        if(puntos.empty()) {
+            cout << "No hay puntos en el plano\n";
+            return;
+        }
+
+        Punto masCercano = puntos[0];
+        double minDist2 = pow(masCercano.x - x, 2) + pow(masCercano.y - y, 2);
+
+        for(const auto& p : puntos) {
+            double dist2 = pow(p.x - x, 2) + pow(p.y - y, 2);
+            if(dist2 < minDist2) {
+                minDist2 = dist2;
+                masCercano = p;
+            }
+        }
+
+        cout << "Punto mas cercano a (" << x << "," << y << ") es (" 
+             << masCercano.x << "," << masCercano.y << ") con distancia " 
+             << fixed << setprecision(2) << sqrt(minDist2) << endl;
+    }
+};
+
+// Limpiar pantalla
+void clean() {
+    system("cls");
 }
 
 int main() {
+    Plano plano;
+    int opcion;
 
-    int maxX = 20;
-    int maxY = 10;
+    do {
+        clean();
+        cout << "==== MENU PLANO CARTESIANO ====\n";
+        cout << "1. Definir limites del plano\n";
+        cout << "2. Agregar punto\n";
+        cout << "3. Mostrar plano\n";
+        cout << "4. Punto mas cercano\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione opcion: ";
+        cin >> opcion;
 
-    int origenX = 5;
-    int origenY = 15;
+        switch(opcion) {
+            case 1: {
+                int x, y;
+                cout << "Ingrese limite X: ";
+                cin >> x;
+                cout << "Ingrese limite Y: ";
+                cin >> y;
+                plano.limites(x, y);
+                break;
+            }
+            case 2: {
+                int px, py;
+                cout << "Ingrese coordenada X del punto: ";
+                cin >> px;
+                cout << "Ingrese coordenada Y del punto: ";
+                cin >> py;
+                plano.agregarPunto(px, py);
+                plano.draw();
+                cout << "\nPunto agregado. Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 3: {
+                clean();
+                plano.draw();
+                cout << "\nPresione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 4: {
+                int px, py;
+                cout << "Ingrese coordenada X de referencia: ";
+                cin >> px;
+                cout << "Ingrese coordenada Y de referencia: ";
+                cin >> py;
+                plano.pntCercano(px, py);
+                cout << "Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+            }
+            case 5:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opcion invalida. Presione enter para continuar...";
+                cin.ignore();
+                cin.get();
+                break;
+        }
 
-    // Dibujar eje X
-    for(int x = 0; x <= maxX; x++) {
-        gotoxy(origenX + x, origenY);
-        cout << "-";
-    }
+    } while(opcion != 5);
 
-    // Dibujar eje Y
-    for(int y = 0; y <= maxY; y++) {
-        gotoxy(origenX, origenY - y);
-        cout << "|";
-    }
-
-    gotoxy(origenX, origenY);
-    cout << "+";
-
-    // Dibujar puntos
-    punto(5, 3, origenX, origenY);
-    punto(10, 7, origenX, origenY);
-    punto(15, 2, origenX, origenY);
-
-    gotoxy(0, origenY + 3);
     return 0;
 }
